ac75e5ef629d833a610cf8689f0925428f57ad46
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-01-17 10:44:43 +0100

    - Replace DB_URL with DB_PATH and set it to the home directory

--- database/src/main/java/com/timehub/configuration/DatabaseConfig.java
+++ database/src/main/java/com/timehub/configuration/DatabaseConfig.java
@@ -5,7 +5,16 @@ package com.timehub.configuration;
  * configuration is handled in this class.
  */
 public class DatabaseConfig {
-  public static final String DB_URL = "jdbc:sqlite:localdata/app.db";
+  public static final String DB_PATH;
   public static final int NUMBER_OF_CONNECTION_RETRIES = 4;
   public static final int DURATION_TO_WAIT_BETWEEN_RETRIES = 10;
+
+  static {
+    String userHome = System.getProperty("user.home");
+    DB_PATH = userHome + "/.timehub/app.db";
+  }
+
+  public static String getDatabaseUrl() {
+    return "jdbc:sqlite:" + DB_PATH;
+  }
 }
