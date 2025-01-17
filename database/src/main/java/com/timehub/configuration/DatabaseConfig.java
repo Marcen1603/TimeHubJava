9ca5a90d7da5d49356271d60288e57786118ba14
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-01-17 10:16:39 +0100

    - Add new parameters to DatabaseConfig

--- database/src/main/java/com/timehub/configuration/DatabaseConfig.java
+++ database/src/main/java/com/timehub/configuration/DatabaseConfig.java
@@ -6,4 +6,6 @@ package com.timehub.configuration;
  */
 public class DatabaseConfig {
   public static final String DB_URL = "jdbc:sqlite:localdata/app.db";
+  public static final int NUMBER_OF_CONNECTION_RETRIES = 4;
+  public static final int DURATION_TO_WAIT_BETWEEN_RETRIES = 10;
 }
