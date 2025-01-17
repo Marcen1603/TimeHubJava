5610974eca6a9a58cabea36a9431db27655212d9
Marcel Peplies <marcel.peplies@uni-oldenburg.de>
2025-01-17 09:40:58 +0100

    - Create DatabaseConfig - Add sqlite to pom

---
+++ database/src/main/java/com/timehub/configuration/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package com.timehub.configuration;
+
+/**
+ * Different configuration parameters are necessary for the underlying database. The part of this
+ * configuration is handled in this class.
+ */
+public class DatabaseConfig {
+  public static final String DB_URL = "jdbc:sqlite:localdata/app.db";
+}
