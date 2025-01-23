f23e3fb669ebf9f56a58cd0ba6e17d5fd9b670c1
Marcel Peplies <mpeplies03@gmail.com>
2025-01-23 23:55:51 +0100

    Change main file in databse

--- database/src/main/java/com/timehub/Main.java
+++ database/src/main/java/com/timehub/Main.java
@@ -1,18 +1,16 @@
 package com.timehub;
 
+import com.timehub.CRUD.TicketDatabaseCRUD;
 import com.timehub.configuration.DatabaseConnection;
 
 import java.sql.Connection;
-
-import static com.timehub.configuration.CRUD.DatabaseCRUD.createTables;
-import static com.timehub.configuration.CRUD.DatabaseCRUD.listTables;
 
 public class Main {
     public static void main(String[] args) {
 
         Connection connection = DatabaseConnection.getConnection();
-        createTables(connection);
-        listTables(connection);
+        TicketDatabaseCRUD ticketDatabaseCRUD = new TicketDatabaseCRUD(connection);
+        ticketDatabaseCRUD.createTable();
 
     }
 }
