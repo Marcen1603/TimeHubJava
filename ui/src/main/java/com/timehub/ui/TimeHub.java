d454cca2000944086687cc3b059ccf31d4f451d0
Marcel Peplies <mpeplies03@gmail.com>
2025-01-31 23:03:05 +0100

    Update poms to start the app

--- ui/src/main/java/com/timehub/ui/TimeHub.java
+++ ui/src/main/java/com/timehub/ui/TimeHub.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TimeHub extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/Dashboard_UI.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TimeHub.class.getResource("fxml/Dashboard_UI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("TimeHub - Java Version");
         stage.setScene(scene);
         stage.show();
     }
